df6f58c0e125424372d785ffe806a975f4dba45d
JuanJC177 <@JuanJC177>
2025-11-13 20:56:07 -0500

    agrego el vercel.json

--- backend/db.py
+++ backend/db.py
@@ -3,10 +3,15 @@ import psycopg2
 import os
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("PGHOST", "localhost"),
-        port=int(os.getenv("PGPORT", 5432)),
-        database=os.getenv("PGDATABASE", "proyect13"),
-        user=os.getenv("PGUSER", "postgres"),
-        password=os.getenv("PGPASSWORD", "921535")
-    )
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("PGHOST", "localhost"),
+            port=int(os.getenv("PGPORT", 5432)),
+            database=os.getenv("PGDATABASE", "proyect13"),
+            user=os.getenv("PGUSER", "postgres"),
+            password=os.getenv("PGPASSWORD", "921535")
+        )
+        return conn
+    except Exception as e:
+        print("Error de conexión a la DB:", e)
+        raise
